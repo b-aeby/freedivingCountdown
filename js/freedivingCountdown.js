e0125ace03f76fed893680fd308123ad478a1aa0
b-aeby <benjamin@b-aeby.ch>
2025-01-22 13:12:56 +0100

    mp3 + delay

--- js/freedivingCountdown.js
+++ js/freedivingCountdown.js
@@ -30,260 +30,124 @@ const official_times = ["2 minutes to official top",
     "start cancelled"]
 
 var muted = false;
+const delay = 500;
 
-const synth = window.speechSynthesis;
+const otm120 = new Audio(src="audio/otm120.mp3", type="audio/mp3")
+const otm90 = new Audio(src="audio/otm90.mp3", type="audio/mp3")
+const otm60 = new Audio(src="audio/otm60.mp3", type="audio/mp3")
+const otm30 = new Audio(src="audio/otm30.mp3", type="audio/mp3")
+const otm20 = new Audio(src="audio/otm20.mp3", type="audio/mp3")
+const otm10 = new Audio(src="audio/otm10.mp3", type="audio/mp3")
+const otm5 = new Audio(src="audio/otm5.mp3", type="audio/mp3")
+const otm4 = new Audio(src="audio/otm4.mp3", type="audio/mp3")
+const otm3 = new Audio(src="audio/otm3.mp3", type="audio/mp3")
+const otm2 = new Audio(src="audio/otm2.mp3", type="audio/mp3")
+const otm1 = new Audio(src="audio/otm1.mp3", type="audio/mp3")
+const ot = new Audio(src="audio/ot.mp3", type="audio/mp3")
+const otp1 = new Audio(src="audio/otp1.mp3", type="audio/mp3")
+const otp2 = new Audio(src="audio/otp2.mp3", type="audio/mp3")
+const otp3 = new Audio(src="audio/otp3.mp3", type="audio/mp3")
+const otp4 = new Audio(src="audio/otp4.mp3", type="audio/mp3")
+const otp5 = new Audio(src="audio/otp5.mp3", type="audio/mp3")
+const otp6 = new Audio(src="audio/otp6.mp3", type="audio/mp3")
+const otp7 = new Audio(src="audio/otp7.mp3", type="audio/mp3")
+const otp8 = new Audio(src="audio/otp8.mp3", type="audio/mp3")
+const otp9 = new Audio(src="audio/otp9.mp3", type="audio/mp3")
+const otp10 = new Audio(src="audio/otp10.mp3", type="audio/mp3")
+const otp20 = new Audio(src="audio/otp20.mp3", type="audio/mp3")
+const otp25 = new Audio(src="audio/otp25.mp3", type="audio/mp3")
+const otp26 = new Audio(src="audio/otp26.mp3", type="audio/mp3")
+const otp27 = new Audio(src="audio/otp27.mp3", type="audio/mp3")
+const otp28 = new Audio(src="audio/otp28.mp3", type="audio/mp3")
+const otp29 = new Audio(src="audio/otp29.mp3", type="audio/mp3")
+const otp30 = new Audio(src="audio/otp30.mp3", type="audio/mp3")
+const otp31 = new Audio(src="audio/otp31.mp3", type="audio/mp3")
 
-const otm120 = new SpeechSynthesisUtterance("2 minutes to official top");
-const otm90 = new SpeechSynthesisUtterance("1 30");
-const otm60 = new SpeechSynthesisUtterance("1 minute");
-const otm30 = new SpeechSynthesisUtterance("30 seconds");
-const otm20 = new SpeechSynthesisUtterance("20");
-const otm10 = new SpeechSynthesisUtterance("10");
-const otm5 = new SpeechSynthesisUtterance("5");
-const otm4 = new SpeechSynthesisUtterance("4");
-const otm3 = new SpeechSynthesisUtterance("3");
-const otm2 = new SpeechSynthesisUtterance("2");
-const otm1 = new SpeechSynthesisUtterance("1");
-const ot = new SpeechSynthesisUtterance("Official top");
-const otp1 = new SpeechSynthesisUtterance("Plus 1");
-const otp2 = new SpeechSynthesisUtterance("2");
-const otp3 = new SpeechSynthesisUtterance("3");
-const otp4 = new SpeechSynthesisUtterance("4");
-const otp5 = new SpeechSynthesisUtterance("5");
-const otp6 = new SpeechSynthesisUtterance("6");
-const otp7 = new SpeechSynthesisUtterance("7");
-const otp8 = new SpeechSynthesisUtterance("8");
-const otp9 = new SpeechSynthesisUtterance("9");
-const otp10 = new SpeechSynthesisUtterance("10");
-const otp20 = new SpeechSynthesisUtterance("20");
-const otp25 = new SpeechSynthesisUtterance("25");
-const otp26 = new SpeechSynthesisUtterance("26");
-const otp27 = new SpeechSynthesisUtterance("27");
-const otp28 = new SpeechSynthesisUtterance("28");
-const otp29 = new SpeechSynthesisUtterance("29");
-const otp30 = new SpeechSynthesisUtterance("30");
-const otp31 = new SpeechSynthesisUtterance("Start cancelled");
+otm120.preload = 'auto';
+otm120.play();
+otm120.pause();
 
-otm120.voice = synth.getVoices()[0];
-otm120.pitch = 1;
-otm120.rate = 1;
-otm120.lang = "en-US"
+otm90.preload = 'auto';
+otm60.preload = 'auto';
+otm30.preload = 'auto';
+otm20.preload = 'auto';
+otm10.preload = 'auto';
+otm5.preload = 'auto';
+otm4.preload = 'auto';
+otm3.preload = 'auto';
+otm2.preload = 'auto';
+otm1.preload = 'auto';
+ot.preload = 'auto';
+otp1.preload = 'auto';
+otp2.preload = 'auto';
+otp3.preload = 'auto';
+otp4.preload = 'auto';
+otp5.preload = 'auto';
+otp6.preload = 'auto';
+otp7.preload = 'auto';
+otp7.play();
+otp7.pause();
 
-otm90.voice = synth.getVoices()[0];
-otm90.pitch = 1;
-otm90.rate = 1;
-otm90.lang = "en-US"
-
-otm60.voice = synth.getVoices()[0];
-otm60.pitch = 1;
-otm60.rate = 1;
-otm60.lang = "en-US"
-
-otm30.voice = synth.getVoices()[0];
-otm30.pitch = 1;
-otm30.rate = 1;
-otm30.lang = "en-US"
-
-otm20.voice = synth.getVoices()[0];
-otm20.pitch = 1;
-otm20.rate = 1;
-otm20.lang = "en-US"
-
-otm10.voice = synth.getVoices()[0];
-otm10.pitch = 1;
-otm10.rate = 1;
-otm10.lang = "en-US"
-
-otm5.voice = synth.getVoices()[0];
-otm5.pitch = 1;
-otm5.rate = 1;
-otm5.lang = "en-US"
-
-otm4.voice = synth.getVoices()[0];
-otm4.pitch = 1;
-otm4.rate = 1;
-otm4.lang = "en-US"
-
-otm3.voice = synth.getVoices()[0];
-otm3.pitch = 1;
-otm3.rate = 1;
-otm3.lang = "en-US"
-
-otm2.voice = synth.getVoices()[0];
-otm2.pitch = 1;
-otm2.rate = 1;
-otm2.lang = "en-US"
-
-otm1.voice = synth.getVoices()[0];
-otm1.pitch = 1;
-otm1.rate = 1;
-otm1.lang = "en-US"
-
-ot.voice = synth.getVoices()[0];
-ot.pitch = 1;
-ot.rate = 1;
-ot.lang = "en-US"
-
-otp1.voice = synth.getVoices()[0];
-otp1.pitch = 1;
-otp1.rate = 1;
-otp1.lang = "en-US"
-
-otp2.voice = synth.getVoices()[0];
-otp2.pitch = 1;
-otp2.rate = 1;
-otp2.lang = "en-US"
-
-otp3.voice = synth.getVoices()[0];
-otp3.pitch = 1;
-otp3.rate = 1;
-otp3.lang = "en-US"
-
-otp4.voice = synth.getVoices()[0];
-otp4.pitch = 1;
-otp4.rate = 1;
-otp4.lang = "en-US"
-
-otp5.voice = synth.getVoices()[0];
-otp5.pitch = 1;
-otp5.rate = 1;
-otp5.lang = "en-US"
-
-otp6.voice = synth.getVoices()[0];
-otp6.pitch = 1;
-otp6.rate = 1;
-otp6.lang = "en-US"
-
-otp7.voice = synth.getVoices()[0];
-otp7.pitch = 1;
-otp7.rate = 1;
-otp7.lang = "en-US"
-
-otp8.voice = synth.getVoices()[0];
-otp8.pitch = 1;
-otp8.rate = 1;
-otp8.lang = "en-US"
-
-otp9.voice = synth.getVoices()[0];
-otp9.pitch = 1;
-otp9.rate = 1;
-otp9.lang = "en-US"
-
-otp10.voice = synth.getVoices()[0];
-otp10.pitch = 1;
-otp10.rate = 1;
-otp10.lang = "en-US"
-
-otp20.voice = synth.getVoices()[0];
-otp20.pitch = 1;
-otp20.rate = 1;
-otp20.lang = "en-US"
-
-otp25.voice = synth.getVoices()[0];
-otp25.pitch = 1;
-otp25.rate = 1;
-otp25.lang = "en-US"
-
-otp26.voice = synth.getVoices()[0];
-otp26.pitch = 1;
-otp26.rate = 1;
-otp26.lang = "en-US"
-
-otp27.voice = synth.getVoices()[0];
-otp27.pitch = 1;
-otp27.rate = 1;
-otp27.lang = "en-US"
-
-otp28.voice = synth.getVoices()[0];
-otp28.pitch = 1;
-otp28.rate = 1;
-otp28.lang = "en-US"
-
-otp29.voice = synth.getVoices()[0];
-otp29.pitch = 1;
-otp29.rate = 1;
-otp29.lang = "en-US"
-
-otp30.voice = synth.getVoices()[0];
-otp30.pitch = 1;
-otp30.rate = 1;
-otp30.lang = "en-US"
-
-otp31.voice = synth.getVoices()[0];
-otp31.pitch = 1;
-otp31.rate = 1;
-otp31.lang = "en-US"
-
-// const speak = function (text) {
-//     if (!muted) {
-//     let utterance = new SpeechSynthesisUtterance(text);
-//     utterance.lang = "en-US";
-//     window.speechSynthesis.cancel();
-//     speechSynthesis.speak(utterance);
-//     }
-// }
+otp8.preload = 'auto';
+otp9.preload = 'auto';
+otp10.preload = 'auto';
+otp20.preload = 'auto';
+otp25.preload = 'auto';
+otp26.preload = 'auto';
+otp27.preload = 'auto';
+otp28.preload = 'auto';
+otp29.preload = 'auto';
+otp30.preload = 'auto';
+otp31.preload = 'auto';
 
 var timer = new easytimer.Timer();
 
 timer.addEventListener('secondsUpdated', function (e) {
-    $('#basicUsage').html(timer.getTimeValues().toString());
+    // $('#basicUsage').html(timer.getTimeValues().toString());
+    // Add timeout delay to refresh display?
+    setTimeout(function(){
+        $('#countdownExample .values').html(timer.getTimeValues().toString());
+    }, delay);
+    
     var timeplus = timer.getTotalTimeValues().seconds;
     if (timeplus === 31) {
-        synth.cancel();
-        synth.speak(otp31);
+        otp31.play();
         timer.stop();
         // timer.reset();
     } else if (timeplus === 30) {
-        synth.cancel();
-        synth.speak(otp30)
+        otp30.play();
     } else if (timeplus === 29) {
-        synth.cancel();
-        synth.speak(otp29)
+        otp29.play();
     } else if (timeplus === 28) {
-        synth.cancel();
-        synth.speak(otp28)
+        otp28.play();
     } else if (timeplus === 27) {
-        synth.cancel();
-        synth.speak(otp27)
+        otp27.play();
     } else if (timeplus === 26) {
-        synth.cancel();
-        synth.speak(otp26)
+        otp26.play();
     } else if (timeplus === 25) {
-        synth.cancel();
-        synth.speak(otp25)
+        otp25.play();
     } else if (timeplus === 20) {
-        synth.cancel();
-        synth.speak(otp20)
+        otp20.play();
     } else if (timeplus === 10) {
-        synth.cancel();
-        synth.speak(otp10)
+        otp10.play();
     } else if (timeplus === 9) {
-        synth.cancel();
-        synth.speak(otp9)
+        otp9.play();
     } else if (timeplus === 8) {
-        synth.cancel();
-        synth.speak(otp8)
+        otp8.play();
     } else if (timeplus === 7) {
-        synth.cancel();
-        synth.speak(otp7)
+        otp7.play;
     } else if (timeplus === 6) {
-        synth.cancel();
-        synth.speak(otp6)
+        otp6.play();
     } else if (timeplus === 5) {
-        synth.cancel();
-        synth.speak(otp5)
+        otp5.play();
     } else if (timeplus === 4) {
-        synth.cancel();
-        synth.speak(otp4)
+        otp4.play();
     } else if (timeplus === 3) {
-        synth.cancel();
-        synth.speak(otp3)
+        otp3.play();
     } else if (timeplus === 2) {
-        synth.cancel();
-        synth.speak(otp2)
+        otp2.play();
     } else if (timeplus === 1) {
-        synth.cancel();
-        synth.speak(otp1)
+        otp1.play();
     } else {
         console.log(timeplus);
     }
@@ -292,45 +156,41 @@ timer.addEventListener('secondsUpdated', function (e) {
 var timer_countdown = new easytimer.Timer();
 
 timer_countdown.addEventListener('secondsUpdated', function (e) {
-    $('#countdownExample .values').html(timer_countdown.getTimeValues().toString());
     var timeleft = timer_countdown.getTotalTimeValues().seconds;
+
+    setTimeout(function(){
+        $('#countdownExample .values').html(timer_countdown.getTimeValues().toString());
+    }, delay);
+
     if (timeleft === 120) {
-        synth.cancel();
-        synth.speak(otm120);
+        otm120.play();
         console.log("2 minutes to official top");
     } else if (timeleft === 90) {
-        synth.cancel();
-        synth.speak(otm90);
+        otm90.play();
     } else if (timeleft === 60) {
-        synth.cancel();
-        synth.speak(otm60);
+        otm60.play();
     } else if (timeleft === 30) {
-        synth.cancel();
-        synth.speak(otm30)
+        otm30.play();
     } else if (timeleft === 20) {
-        synth.cancel();
-        synth.speak(otm20)
+        otm20.play();
     } else if (timeleft === 10) {
-        synth.cancel();
-        synth.speak(otm10)
+        otm10.play();
     } else if (timeleft === 5) {
-        synth.cancel();
-        synth.speak(otm5)
+        otm5.play();
     } else if (timeleft === 4) {
-        synth.cancel();
-        synth.speak(otm4)
+        otm4.play();
     } else if (timeleft === 3) {
-        synth.cancel();
-        synth.speak(otm3)
+        otm3.play();
     } else if (timeleft === 2) {
-        synth.cancel();
-        synth.speak(otm2)
+        otm2.play();
     } else if (timeleft === 1) {
-        synth.cancel();
-        synth.speak(otm1)
+        otm1.play();
     } else {
         console.log(timeleft);
     }
+
+    
+    
 
 });
 
@@ -338,8 +198,7 @@ timer_countdown.addEventListener('secondsUpdated', function (e) {
 timer_countdown.addEventListener('targetAchieved', function (e) {
     $('#countdownExample .values').html('Official top');
     // speak('official top')
-    synth.cancel();
-    synth.speak(ot);
+    ot.play();
     timer.start();
 });
 
