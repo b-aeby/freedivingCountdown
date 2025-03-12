c5dd2e1bdae3114e85eb9176b99ce96de22d5216
b-aeby <benjamin@b-aeby.ch>
2025-03-12 21:16:31 +0100

    demo active

--- js/freedivingCountdown.js
+++ js/freedivingCountdown.js
@@ -57,6 +57,120 @@ audio_28.preload = 'auto';
 audio_29.preload = 'auto';
 audio_30.preload = 'auto';
 
+
+// Declaration
+class aidaCountdown {
+    constructor(year, month, day, hour, minute, second) {
+        this.startTime = new Date(year, month, day, hour, minute, second);
+        this.timer = new easytimer.Timer();
+        this.timer.addEventListener('secondsUpdated', function (e) {
+            // $('#basicUsage').html(timer.getTimeValues().toString());
+            // Add timeout delay to refresh display?
+            setTimeout(function () {
+                $('.countdown').html(timer.getTimeValues().toString());
+
+            }, delay);
+
+            var timeplus = timer.getTotalTimeValues().seconds;
+            if (timeplus === 31) {
+                audio_start_cancelled.play();
+                timer.stop();
+                // timer.reset();
+            } else if (timeplus === 30) {
+                audio_30.play();
+            } else if (timeplus === 29) {
+                audio_29.play();
+            } else if (timeplus === 28) {
+                audio_28.play();
+            } else if (timeplus === 27) {
+                audio_27.play();
+            } else if (timeplus === 26) {
+                audio_26.play();
+            } else if (timeplus === 25) {
+                audio_25.play();
+            } else if (timeplus === 20) {
+                audio_20.play();
+            } else if (timeplus === 10) {
+                audio_10.play();
+            } else if (timeplus === 9) {
+                audio_9.play();
+            } else if (timeplus === 8) {
+                audio_8.play();
+            } else if (timeplus === 7) {
+                audio_7.play();
+            } else if (timeplus === 6) {
+                audio_6.play();
+            } else if (timeplus === 5) {
+                audio_5.play();
+            } else if (timeplus === 4) {
+                audio_4.play();
+            } else if (timeplus === 3) {
+                audio_3.play();
+            } else if (timeplus === 2) {
+                audio_2.play();
+            } else if (timeplus === 1) {
+                audio_plus_1.play();
+            } else {
+                console.log(timeplus);
+            }
+        });
+
+        this.timer_countdown = new easytimer.Timer();
+        this.timer_countdown.addEventListener('secondsUpdated', function (e) {
+            var timeleft = timer_countdown.getTotalTimeValues().seconds;
+
+            setTimeout(function () {
+                $('.countdown').html(timer_countdown.getTimeValues().toString());
+            }, delay);
+
+            if (timeleft === 120) {
+                audio_m120.play();
+                console.log("2 minutes to official top");
+            } else if (timeleft === 90) {
+                audio_m90.play();
+            } else if (timeleft === 60) {
+                audio_m60.play();
+            } else if (timeleft === 30) {
+                audio_m30.play();
+            } else if (timeleft === 20) {
+                audio_20.play();
+            } else if (timeleft === 10) {
+                audio_10.play();
+            } else if (timeleft === 5) {
+                audio_5.play();
+            } else if (timeleft === 4) {
+                audio_4.play();
+            } else if (timeleft === 3) {
+                audio_3.play();
+            } else if (timeleft === 2) {
+                audio_2.play();
+            } else if (timeleft === 1) {
+                audio_1.play();
+            } else {
+                console.log(timeleft);
+            }
+        });
+
+        this.timer_countdown.addEventListener('targetAchieved', function (e) {
+            $('#countdownExample .values').html('Official top');
+            // speak('official top')
+            audio_ot.play();
+            timer.start();
+        });
+        console.log(this.startTime.toString());
+    }
+
+    // Getter
+    get get_startTime() {
+        return this.startTime.toString();
+    }
+
+    //TODO: add function to start countdown at startTime or in (startTime - now) seconds
+
+
+}
+
+
 var timer = new easytimer.Timer();
 
 timer.addEventListener('secondsUpdated', function (e) {
@@ -157,7 +271,7 @@ timer_countdown.addEventListener('targetAchieved', function (e) {
 });
 
 const freedivingCountdown = function (sec) {
-    timer_countdown.start({ countdown: true, startValues: { minutes: 2, seconds: 60-sec } });
+    timer_countdown.start({ countdown: true, startValues: { minutes: 2, seconds: 60 - sec } });
 }
 
 
@@ -184,10 +298,10 @@ function display_clock() {
     }
     const time = `${hours}:${minutes_str}:${seconds_str}`;
 
-    setTimeout(function(){
+    setTimeout(function () {
         $('.clock').html(time);
     }, delay)
-    
+
     if (not_started){
         freedivingCountdown(seconds);
         not_started = false;
