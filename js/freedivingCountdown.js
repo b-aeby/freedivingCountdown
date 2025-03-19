09b2cd39aa972090cc2686b098efe45c1aff702a
Benjamin Aeby <58775333+b-aeby@users.noreply.github.com>
2025-03-19 09:17:52 +0100

    Update freedivingCountdown.js

--- js/freedivingCountdown.js
+++ js/freedivingCountdown.js
@@ -108,7 +108,7 @@ class aidaCountdown {
         // console.log('countDownSecond!', this);
         const timeleft = this.timer_countdown.getTotalTimeValues().seconds;
 
-        if (timeleft < 6 * 60) {
+        if (timeleft < 5 * 60) {
             timer_display = this.timer_countdown.getTimeValues().toString();
             if(muted){
                 toggle_mute();
