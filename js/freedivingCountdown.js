6f3edcc199cd40c724c3f786c138a20d710f3e8e
baeby <benjamin@b-aeby.ch>
2025-11-06 19:46:45 +0100

    add white noise and announce for 3 minutes to ot

--- js/freedivingCountdown.js
+++ js/freedivingCountdown.js
@@ -126,15 +126,15 @@ class aidaCountdown {
         }
 
         // if (timeleft === 310 || timeleft === 250 || timeleft === 190 || timeleft === 130 || timeleft === 100 || timeleft === 70 || timeleft === 40) {
-        if (timeleft === 130 || timeleft === 100 || timeleft === 70 || timeleft === 40) {
+        if (timeleft === 190 || timeleft === 130 || timeleft === 100 || timeleft === 70 || timeleft === 40) {
             // console.log("White noise to wake up audio device");
             play_audio(audio_wn);
         // } else if (timeleft === 300) {
         //     play_audio(audio_m5min);
         // } else if (timeleft === 240) {
         //     play_audio(audio_m4min);
-        // } else if (timeleft === 180) {
-        //     play_audio(audio_m3min);
+        } else if (timeleft === 180) {
+            play_audio(audio_m3min);
         } else if (timeleft === 120) {
             play_audio(audio_m120);
         } else if (timeleft === 90) {
